2294e5bcb23d8b8635ae5325763f0e144416a9af
CosmicRadiocity <heartbreakersaturn@gmail.com>
2025-02-22 15:38:02 +0100

    add player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+import player
 
 def main():
     pygame.init()
@@ -9,14 +10,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_obj = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
