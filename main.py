35c32fd97f4e6f1706d58b17f010c290d34cc442
CosmicRadiocity <heartbreakersaturn@gmail.com>
2025-02-23 14:43:50 +0100

    implement player rotation

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_obj.update(dt)
         player_obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
